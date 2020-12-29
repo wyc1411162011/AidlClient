a9d17b100ce3732f0565ff73e9e5af282b910e8d
wangychr <wangychr@yongyou.com>
2020-12-29 20:09:25 +0800

    [功能]aidl连接池功能

--- app/src/main/java/com/aidlclient/MainActivity.java
+++ app/src/main/java/com/aidlclient/MainActivity.java
@@ -9,23 +9,29 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.os.RemoteException;
 import android.util.Log;
 import android.view.View;
 
 import com.aidlserver.Book;
 import com.aidlserver.BookController;
+import com.aidlserver.IBinderPool;
+import com.aidlserver.IComputer;
+import com.aidlserver.ISecurityCenter;
 
 import java.util.List;
 
 public class MainActivity extends Activity {
     BookController bookController;
     private int number =0;
+    IBinderPool binderPool;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bindListeners();
         bindService();
+        bindPoolService();
     }
     private void bindListeners(){
         findViewById(R.id.bt_get).setOnClickListener(new View.OnClickListener() {
@@ -59,6 +65,43 @@ public class MainActivity extends Activity {
                 }
             }
         });
+        findViewById(R.id.bt_computer).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(binderPool != null){
+                    try {
+                        IBinder iBinder = binderPool.queryBinder(0);
+                            IComputer computer = IComputer.Stub.asInterface(iBinder);
+                            int number = computer.add(1,2);
+                            Log.e("tag","client端的add的结果"+number);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        findViewById(R.id.bt_securitycenter).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(binderPool != null){
+                    try {
+                        IBinder iBinder = binderPool.queryBinder(1);
+                        ISecurityCenter securityCenter = ISecurityCenter.Stub.asInterface(iBinder);
+                        String result = securityCenter.encrypt("");
+                        Log.e("tag","client 端加密的结果  "+result);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+    private void bindPoolService() {
+        Intent intent = new Intent();
+        intent.setPackage("com.aidlserver");
+        intent.setAction("com.aidlserver.poolservice");
+        intent.addCategory("android.intent.category.DEFAULT");
+        bindService(intent, poolConnection, Context.BIND_AUTO_CREATE);
     }
     private void bindService() {
         Intent intent = new Intent();
@@ -70,6 +113,7 @@ public class MainActivity extends Activity {
     private ServiceConnection serviceConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
+            //把binder转成操作类
              bookController = BookController.Stub.asInterface(service);
         }
 
@@ -77,4 +121,17 @@ public class MainActivity extends Activity {
         public void onServiceDisconnected(ComponentName name) {
         }
     };
+    private ServiceConnection poolConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            //把binder转成操作类
+            binderPool = IBinderPool.Stub.asInterface(service);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+        }
+    };
+
+
 }
